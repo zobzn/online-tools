7ac89d1eb0c1df848f82a4cae606b3d73f3241ad
zema <zlobzn@gmail.com>
2019-09-07 17:35:56 +0300

    added autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import autoprefixer from "autoprefixer";
+import Textarea from "react-textarea-autosize";
+import postcss from "postcss";
+import "./App.css";
+
+// (await import("es6-promise")).polyfill();
+
+const defaultAutoprefixerSrc = `::placeholder {
+  color: gray;
+}`;
+
+const autoprefixer_config = {
+  grid: true,
+  overrideBrowserslist: [
+    // Country statistics is not supported in client-side build of Browserslist
+    // '> 10% in RU',
+    // '> 10% in UA',
+    "last 6 versions",
+    "ff ESR",
+    "opera >= 12",
+    "safari >= 5",
+    "ios >= 8",
+    "ie >= 8"
+  ]
+};
+
+function addPrefixes(css) {
+  return postcss([autoprefixer(autoprefixer_config)])
+    .process(css, { from: undefined })
+    .then(function(result) {
+      return result.css;
+    });
+}
+
+function Autoprefixer() {
+  const [css, setCss] = useState(defaultAutoprefixerSrc);
+
+  function handleChange(e) {
+    setCss(e.target.value);
+  }
+
+  function handleAddPrefixes() {
+    addPrefixes(css).then(css => setCss(css));
+  }
+
+  return (
+    <div className="tool-autoprefixer">
+      <p>
+        <button onClick={handleAddPrefixes}>add prefixes</button>
+      </p>
+      <p>
+        <Textarea value={css} onChange={handleChange} />
+      </p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <h3>Autoprefixer</h3>
+        <Autoprefixer />
+      </div>
+      <hr />
+      <div></div>
     </div>
   );
 }
